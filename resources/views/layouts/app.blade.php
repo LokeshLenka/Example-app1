0cee7e17f5329080765eb9629e9d44c5c095d302
LokeshLenka <lenkalokesh98@gmail.com>
2024-12-27 17:15:37 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,10 +23,10 @@
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=more_vert" />
-<!--     <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet"/> -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet"/>
 
-<!--     <script src ="{{ asset('js/app.ja') }}"></script> -->
+    <script src ="{{ asset('js/app.js') }}"></script>
 <!--     <script src="https://cdn.tailwindcss.com"></script> -->
 <!--       <script>
         tailwind.config = {
@@ -64,7 +64,7 @@
             /* Increase or decrease as needed */
         }
     </style>
-@viteReactRefresh
+<!-- @viteReactRefresh -->
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 {{-- @section('content') --}}
