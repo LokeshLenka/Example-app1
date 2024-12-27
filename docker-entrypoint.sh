e33ea541ff6d5d2c07b86791007b2265ae24074c
LokeshLenka <lenkalokesh98@gmail.com>
2024-12-27 17:59:06 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-# Wait for database if needed (optional)
-# while ! nc -z db 3306; do
-#   echo "Waiting for database connection..."
-#   sleep 1
-# done
+# # Wait for database if needed (optional)
+# # while ! nc -z db 3306; do
+# #   echo "Waiting for database connection..."
+# #   sleep 1
+# # done
 
-# Run database migrations
-php artisan migrate --force
+# # Run database migrations
+# php artisan migrate --force
 
-# Start supervisor (if you're using it)
-# supervisord -c /etc/supervisor/supervisord.conf
+# # Start supervisor (if you're using it)
+# # supervisord -c /etc/supervisor/supervisord.conf
 
-# Execute CMD
-exec "$@"
+# # Execute CMD
+# exec "$@"
